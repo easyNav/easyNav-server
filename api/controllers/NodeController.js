52b255d609fa70f5739d17727f874258923277cb
Joel Tong <me@joeltong.org>
2014-08-21 21:47:35 +0800

    added mocha test framework / set up grunt watch configured to use blueprint CRUD auto-endroutes added Node for paths ORM

---
+++ api/controllers/NodeController.js
@@ -0,0 +1,10 @@
+/**
+ * NodeController
+ *
+ * @description :: Server-side logic for managing nodes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+};
+
