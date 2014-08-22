8c0f83987ac68be0a1a343f7210ce4e58eff396a
Joel Tong <me@joeltong.org>
2014-08-22 12:45:25 +0800

    pushing before we make major additions

---
+++ tests/unit/controllers/nodesTest.spec.js
@@ -0,0 +1,16 @@
+var AboutController = require('../../../api/controllers/NodeController'),
+    sinon = require('sinon'),
+    assert = require('assert');
+
+describe('The Node Controller', function () {
+    describe('when we load the node page', function () {
+        it ('should render the view', function () {
+            assert.ok(false, 'dummy ok');
+            // var view = sinon.spy();
+            // NodeController.index(null, {
+            //     view: view
+            // });
+            // assert.ok(view.called);
+        });
+    });
+});
