a57fa64ff49d9aa8528847e5e74a493087ffb387
Joel Tong <me@joeltong.org>
2014-09-08 23:19:18 +0800

    mocha test timeout edit

--- tasks/config/mochaTest.js
+++ tasks/config/mochaTest.js
@@ -20,6 +20,7 @@ module.exports = function(grunt) {
 		test: {
 			options: {
 				reporter: 'spec', 
+				timeout: '8000',
 				require: 'coffee-script/register'
 			},
 			src: ['tests/**/*.spec.js', 'tests/**/*.spec.coffee']
